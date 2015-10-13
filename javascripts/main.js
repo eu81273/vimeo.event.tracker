bc56d46456645718c81eabfe79f567389d5de673
AHN JAE-HA <eu81273@gmail.com>
2015-10-13 13:15:24 +0900

    Add source files

---
+++ javascripts/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var app = angular.module('angularApplication', ['angular.panels']);
+
+//add panels
+app.config(['panelsProvider', function (panelsProvider) {
+
+	panelsProvider
+		.add({
+			id: 'menu',
+			position: 'left',
+			size: '500px',
+			templateUrl: 'templates/menu.html',
+			controller: 'menuCtrl'
+		});
+}]);
+
+//default controller
+app.controller('defaultController', ['$scope', '$window', 'panels', function ($scope, $window, panels) {
+
+	$scope.eventLogs = [];
+
+	//Universal Analytics Emulation
+	$window.ga = function () {
+		$scope.eventLogs.push(Array.prototype.slice.call(arguments));
+		$scope.$apply();
+	};
+
+	$scope.menuOpen = function () {
+		$scope.$broadcast('menu');
+	};
+}]);
+
+//menu panel controller
+app.controller('menuCtrl', ['$scope', 'panels', function ($scope, panels) {
+	$scope.$on('menu', function(event, args) {
+		panels.open("menu");
+	});
+}]);
